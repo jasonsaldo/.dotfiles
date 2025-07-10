0a76a36f4d34af194d929f3ae91f564706c903a0
jsaldo <jason.saldo@gmail.com>
2025-07-09 21:39:20 -0400

    remove deploy.sh script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#copy wezterm file from wsl to winhome
-USERNAME=$(id -un)
-if [ "$USERNAME" = "js" ]; then
-  USERNAME="jason"
-fi
-cp .wezterm.lua /mnt/c/Users/$USERNAME/.wezterm.lua
